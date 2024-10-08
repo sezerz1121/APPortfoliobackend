4df86b3ea2d13393e3f1ba3da22dd04c382dd587
sezerz1121 <85015473+sezerz1121@users.noreply.github.com>
2024-10-08 22:47:30 +0530

    Update project.routes.js

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { projectListing,projectAll,messageCreation } from "../controllers/project.controller.js";
+import { projectListing,projectAll,messageCreation,messageCreationTwo } from "../controllers/project.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 
@@ -14,5 +14,6 @@ router.post("/projectlisting",
 );
 router.route("/projectall").get(projectAll)
 router.route("/message").post(messageCreation)
+router.route("/messageTwo").post(messageCreationTwo)
 
 export default router;
